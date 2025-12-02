66fa15351e573f09dc31eb4bc58b3c7362923c39
diego-ac22 <da.acostac1@uniandes.edu.co>
2025-12-02 10:17:58 -0500

    .

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.optimizador.urls')),  # Redirige a tu app
+    path('', include('apps.optimizador.urls')), 
 ]
